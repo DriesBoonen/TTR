e1ca61a5d4efe59faf6969c1770a5026f7ecfb3f
driesboonen <dries.boonen@hotmail.com>
2018-11-07 15:27:38 +0100

    Speler getters + kaart

--- venv/Speler.py
+++ venv/Speler.py
@@ -7,4 +7,35 @@ class Speler:
         self.__missionscomp = missionscomp
         self.__currmissions = currmissions
 
-        
+
+
+        def set_name(self, name):
+            self.__name = name
+
+        def set_age(self, age):
+            self.__age = age
+
+        def set_color(self, color):
+            self.__color = color
+
+        def set_pawnnr(self, pawnnr):
+            self.__pawnnr = pawnnr
+
+        def set_missionscomp(self, missionscomp):
+            self.__missionscomp = missionscomp
+
+        def set_currmissions(self, currmissions):
+            self.__currmissions = currmissions
+
+
+    """"
+    #add card to hand
+        def addCardToHand(self, card):
+            """adds a single card to hand
+            assumes card is a valid choice
+            card: String
+            """
+            if card != None:
+                self.hand[card] += 1
+                
+        """
