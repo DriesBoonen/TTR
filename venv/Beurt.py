61c9b41d28b36f4dc83059b624af8adc704fd687
Toto-Africa <tttt12345@outlook.be>
2018-11-08 13:54:33 +0100

    Commit from Michiel

---
+++ venv/Beurt.py
@@ -0,0 +1,30 @@
+# Michiel
+# Test voor Jan
+
+class Beurt:
+    # Constructor
+    def __init__(self, playerid):
+        self.__playerid = Speler.get_id
+
+    # Getters
+    def get_id(self):
+        #return self.
+
+    def get_player_id(self):
+        #return
+
+    # Setters
+    def set_id(self):
+        # self. =
+
+    def set_player_id(self):
+
+    # Normal methods
+    def swap_mission(self):
+        # Code ruilMissie
+
+    def extra_train_card(self):
+        # Code extraTreinkaart
+
+    def conquer_route(self):
+        # Code routeInnemen
