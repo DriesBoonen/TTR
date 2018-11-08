34ca268bccc034972f80637f534c2dcf82d62b94
driesboonen <dries.boonen@hotmail.com>
2018-11-08 14:02:55 +0100

    Commit update CPU SPeler en Speler
    --Dries

--- venv/CPUSpeler.py
+++ venv/CPUSpeler.py
@@ -1,4 +1,4 @@
 class CPUSpeler(Speler):
-    def __init__(self, name, age, color, pawnnr, missionscomp, currmissions, isCPU):
-        Speler.__init__(self, name, age, color, pawnnr, missionscomp, currmissions):
+    def __init__(self, id, name, age, color, pawnnr, missionscomp, currmissions, isCPU, hand):
+        Speler.__init__(self, id, name, age, color, pawnnr, missionscomp, currmissions, hand):
         self.__isCPU = isCPU
