bcb57bdaf58d2521d7502a574a0d5bdd6ba982a3
Toto-Africa <tttt12345@outlook.be>
2018-11-08 14:16:23 +0100

    Tried to get a static variable (doesn't exist in Python), so that the id of Beurt automatically increments with 1 when a new instance of Beurt is created.

--- venv/Beurt.py
+++ venv/Beurt.py
@@ -1,29 +1,31 @@
 # Michiel
-# Test voor Jan
 
 class Beurt:
+    id  # Does this work? Goal is to have "static variable" which is the same for all instances of Beurt, so that the id always increments by 1
     # Constructor
     def __init__(self, playerid):
+        id += 1
         self.__playerid = Speler.get_id
+        self.__id = id
 
     # Getters
     def get_id(self):
         #return self.
 
-    def get_player_id(self):
+    def get_playerid(self):
         #return
 
     # Setters
     def set_id(self):
         # self. =
 
-    def set_player_id(self):
+    def set_playerid(self):
 
     # Normal methods
     def swap_mission(self):
         # Code ruilMissie
 
-    def extra_train_card(self):
+    def extra_traincard(self):
         # Code extraTreinkaart
 
     def conquer_route(self):
