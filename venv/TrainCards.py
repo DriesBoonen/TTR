09299968138ae76a2cef4acb9a50a9e0e18b0e4d
ElmerSwaenen <elmerswaenen@gmail.com>
2018-11-14 15:24:01 +0100

    TrainCards aangepast/verbeterdÂ

--- venv/TrainCards.py
+++ venv/TrainCards.py
@@ -3,22 +3,15 @@ import random
 
 class TrainCards:
 
-    def __init__(self, sizeDrawPile):
-        self.sizeDrawPile = sizeDrawPile
+    def __init__(self):
         self.colors = ["black", "white", "green", "blue", "red", "yellow"]
 
         #wildcards nog toevoegen
-        self.cards = ["wild" for i in range(6)] + [i for i in self.possibleColors for j in range(66)]
+        self.cards = ["wild" for i in range(6)] + [i for i in self.colors for j in range(66)]
         self.shuffle(self.cards)
 
 
-
-
-
-
-
     def shuffle(self, cards):
-        cards:list
         random.shuffle(cards)
 
 
@@ -30,12 +23,6 @@ class TrainCards:
         except IndexError:
             #SPEL STOPPEN
             print("Tis gedaan")
-
-
-    def extraCards(self, numberOfTickets):
-        numberOfTickets: int
-
-        return(self.dealCard() for i in range(numberOfTickets))
 
 
     def cardsLeftover(self):
