821915c8cc4cd5af70ba55a70b3716e8c27fc6cb
driesboonen <dries.boonen@hotmail.com>
2018-12-01 13:10:17 +0100

    CommitGUI

--- venv/GUI.py
+++ venv/GUI.py
@@ -7,47 +7,6 @@ from Tkinter import *
 
 class GUI:
 
-
-    def __init__(self):
-
-
-    def onlynum(S):
-        if S in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            return True
-
-        return False
-
-    def start(self, start):
-        # eerst namen ingeven: spelers aanmaken dus
-
-        master = Tk()
-
-        Label(master, text="Speler naam - leeftijd").grid(row=0)
-        Label(master, text="CPU1 naam").grid(row=1)
-        Label(master, text="CPU2 naam").grid(row=1)
-        Label(master, text="CPU3 naam").grid(row=1)
-
-        e1 = Entry(master)
-        e2 = Entry(master)
-        e3 = Entry(master)
-        e4 = Entry(master)
-
-        vcmd = (t3.register(onlynum), '%S')            #enkel num input
-        e5 = Entry(master, validate='key', vcmd=vcmd)  #voor leeftijd
-
-        b1 = Button(master, text="Start Spel", command=buttonstart) #als er op button gedrukt wordt dan voeren we 'buttonstart' uit
-
-        e1.grid(row=0, column=1)
-        e5.grid(row=0, column=2)
-        e2.grid(row=1, column=1)
-        e3.grid(row=2, column=1)
-        e4.grid(row=3, column=1)
-        b1.grid(row=4, column=1)
-
-        mainloop()
-
-
-
     def buttonstart(self):
         # hier dan overgaan naar beurt?
         # hoe doe ik da praktisch? :D
@@ -64,18 +23,53 @@ class GUI:
                     cpu1 = e2.get()
 
                 if len(e4.get()) == 0:
-                    cpu2= "Staf"
+                    cpu2 = "Staf"
                 else:
                     cpu2 = e3.get()
 
                 if len(e3.get()) == 0:
-                    cpu3="Marc"
+                    cpu3 = "Marc"
                 else:
                     cpu3 = e4.get()
 
                 # doorgeven en ga naar beurt dan? hoe doe ik da juist? :D variables zijn dan: (username, age, cpu1-3)
                 # kleuren moeten hier ook nog bij
                 # --> effe hardcoden denk ik
+
+    def start(self):
+        # eerst namen ingeven: spelers aanmaken dus
+
+        master = Tk()
+
+        Label(master, text="Speler naam - leeftijd").grid(row=0)
+        Label(master, text="CPU1 naam").grid(row=1)
+        Label(master, text="CPU2 naam").grid(row=2)
+        Label(master, text="CPU3 naam").grid(row=3)
+
+        e1 = Entry(master)
+        e2 = Entry(master)
+        e3 = Entry(master)
+        e4 = Entry(master)
+
+        ages = []
+
+        for i in range(1, 100):
+            ages.append(i)
+
+        drop = OptionMenu(master, 'Kies leeftijd', *ages)
+
+        #b1 = Button(master, text="Start Spel", command=buttonstart) #als er op button gedrukt wordt dan voeren we 'buttonstart' uit
+
+        e1.grid(row=0, column=1)
+        drop.grid(row=0, column=2)
+        e2.grid(row=1, column=1)
+        e3.grid(row=2, column=1)
+        e4.grid(row=3, column=1)
+        #b1.grid(row=4, column=1)
+
+        mainloop()
+
+
 
 
     def initbord(self):
@@ -126,5 +120,5 @@ class GUI:
 my_gui = GUI()
 
 while True:
-    my_gui.start(self)
+    my_gui.start()
 #dan iets da we kunnen laten samenwerken met route? om de edges(routes) tussen de nodes te kunnen maken
